374918bad0d5c90c38eacc5ba51280cb0d7de462
Thanh Hương <thanhhuong10102005@gmail.com>
2026-09-16 14:56:41 +0700

    add BT1

---
+++ BUOI1/BT1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xuất số chẵn</title>
+</head>
+
+<style>
+    * {
+        font-size: 24px;
+    }
+</style>
+
+<body>
+
+    <?php
+    $so = rand(1, 100);
+
+    echo "<strong>Những số chẵn nằm trong khoảng 1 đến " . $so . " là </strong>";
+    for ($i = 1; $i <= $so; $i++) {
+        if ($i % 2 == 0) {
+            echo "<strong>" . $i . " </strong>";
+        }
+    }
+    ?>
+</body>
+
+</html>
