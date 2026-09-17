3b0f21685d14aa7be9b09e4e0e9bd8937b9aa5ec
Thanh Hương <thanhhuong10102005@gmail.com>
2026-09-17 22:10:42 +0700

    add Shcn.php file (inclass)

---
+++ BUOI2/Shcn.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tính diện tích hình chữ nhật</title>
+</head>
+<style>
+    body {
+        margin: 20px;
+        font-size: 20px;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    form {
+        width: 1200px;
+    }
+
+    .group {
+        margin-bottom: 10px;
+        width: 100%;
+
+    }
+
+    label {
+        display: inline-block;
+        width: 120px;
+        font-weight: bold;
+    }
+
+    input[type="submit"] {
+        width: 100px;
+        padding: 10px;
+        border: none;
+        border-radius: 6px;
+        background-color: #008924;
+        color: white;
+        font-size: 14px;
+        font-weight: bold;
+        cursor: pointer;
+    }
+</style>
+
+<body>
+    <h2>Diện tích hình chữ nhật</h2>
+    <form action="Shcn.php" method="post">
+        <div class="group">
+            <label for="dai">Chiều dài</label>
+            <input type="number" name="dai" min="0.01" step="0.01" value="<?php if (isset($_POST['dai'])) {
+                                                                                echo $_POST["dai"];
+                                                                            } ?>" required>
+        </div>
+
+        <div class="group">
+            <label for="rong">Chiều rộng</label>
+            <input type="number" name="rong" min="0.01" step="0.01" value="<?php if (isset($_POST['rong'])) {
+                                                                                echo $_POST["rong"];
+                                                                            } ?>" required>
+        </div>
+
+        <div class="group">
+            <label for="dientich">Diện tích</label>
+            <input type="number" name="dientich" value="<?php if (isset($_POST["dai"]) && isset($_POST["rong"])) {
+                                                            echo $_POST["dai"] * $_POST["rong"];
+                                                        } ?>" readonly>
+        </div>
+
+        <input type="submit" value="Submit">
+
+    </form>
+</body>
+
+</html>
